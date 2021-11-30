ba30222cc8133dc5b2aadf96f385724301c6947f
Wonhyuk Choi <wc012013@mymail.pomona.edu>
2021-11-30 10:25:45 -0500

    add constant declaration

--- headers/constants.h
+++ headers/constants.h
@@ -15,6 +15,13 @@
 // Frame rate
 #define MAX_FPS 60
 
+#ifndef M_PI
+    #define M_PI 3.14159265358979323846
+#endif // M_PI
+#ifndef M_PI_2
+	#define M_PI_2 (M_PI / 2)
+#endif // M_PI_2
+
 // Cardinal directions
 enum directions
 { LEFT, RIGHT, UP, DOWN };
